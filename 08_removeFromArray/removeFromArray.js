3dd4de590a215f2b7b5545ef8aa382db03bbc159
Walfran <7338683+Walfran@users.noreply.github.com>
2025-05-26 18:29:58 +0000

    all kinds

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...valuesToRemove) {
+    return arr.filter(item => !valuesToRemove.includes(item));
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Check if a and b are numbers and integers
+    if (
+        typeof a !== 'number' || typeof b !== 'number' || 
+        !Number.isInteger(a) || !Number.isInteger(b) || 
+        a < 0 || b < 0
+    ) {
+        return "ERROR";
+    }
 
+    // Make sure a is the smaller number
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+
+    let range = [];
+    for (let i = start; i <= end; i++){
+        range.push(i);
+    }
+
+    const sum = range.reduce((acc, num) => acc + BigInt(num), 0n);
+
+    return sum <= Number.MAX_SAFE_INTEGER ? Number(sum) : sum;
 };
+
+console.log(sumAll(1, 10))
 
 // Do not edit below this line
 module.exports = sumAll;
