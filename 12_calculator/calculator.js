2a1d19249b2a6b212e842498659bf2590d04164c
Walfran <7338683+Walfran@users.noreply.github.com>
2025-06-01 22:26:39 +0000

    big commit

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(function(acc, curr){
+    return acc + curr; 
+  },0); 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(function(acc, curr){
+    return acc * curr;
+  },1); 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = a;
+
+	if (a === 0 || a === 1) {
+    return 1;
+  }
+  while (a > 1) {
+    a--; 
+    result = result * a; 
+  }
+  return result;
 };
 
 // Do not edit below this line
